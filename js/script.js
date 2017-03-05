705c1d27c3d583b5361f7f70d2d54a716d7d44e9
getpona <hello@getpona.com>
2017-03-05 15:40:59 -0600

    Site updated: 2017-03-05 15:40:59

--- js/script.js
+++ js/script.js
@@ -56,18 +56,18 @@
 
   // Caption
   $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('image-link')) return;
+    // $(this).find('img').each(function(){
+    //   if ($(this).parent().hasClass('image-link')) return;
 
-      var alt = this.alt;
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+    //   var alt = this.alt;
+    //   if (alt) $(this).after('<span class="caption">' + alt + '</span>');
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="image-link"></a>');
-    });
+    //   $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="image-link"></a>');
+    // });
 
-    $(this).find('.image-link').each(function(){
-      $(this).attr('rel', 'article' + i);
-    });
+    // $(this).find('.image-link').each(function(){
+    //   $(this).attr('rel', 'article' + i);
+    // });
   });
 
   // Bootstrap table style
